493aeae31a5d56888ed9a435c9a79e6c8b4c6ec6
Alexey Ulyanov <sibuser.nsk@gmail.com>
2013-05-18 20:52:13 +0200

    BUG: Now you can play the first song from your play list.

--- player.py
+++ player.py
@@ -129,9 +129,13 @@ class Player(object):
     If we call this function as event handler we
     assign selected row as current position.
     """
-    def play(self, pos=False):
-        if pos:
+    def play(self, pos=-1):
+
+        if pos != -1:
+            print self.selected
             self.position = self.selected
+
+
         if self.checkStates():
             if self.position == -1:
                 self.chooseFirst()
